a3fbd0c02053f94e1603e2b76cea228b554a2f01
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-17 19:40:14 +0100

    Modified index.js
    Enabled users to prompt in their choice

--- index.js
+++ index.js
@@ -2,18 +2,33 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
+function getComputerChoice(){ //calls a random generated number which converts to a choice
     let ComputerChoice= null;
     let genNum = null;
    genNum = Math.floor(Math.random()* 3) + 1 ;
    console.log(genNum);
 
    if (genNum == 0) {
-   ComputerChoice = "Rock" ;
+   ComputerChoice = "rock" ;
 }   else if(genNum == 1){
    ComputerChoice = "paper" ;
 } else {
-   ComputerChoice = "Scissors" ;
+   ComputerChoice = "scissors" ;
 }
+    console.log(typeof ComputerChoice);
     return ComputerChoice;
 }
+
+function getHumanChoice(){
+    let HumanChoice = null;
+    let input = null; 
+    while (!(input=="rock"||input=="paper"||input=="scissors")){
+     input= prompt("Pick one of the 3 Options listed \n (Rock, Paper or Scissors)") ;
+        input = input.toLowerCase();
+     HumanChoice = input.toLowerCase();}
+
+    console.log(HumanChoice);
+    console.log(typeof HumanChoice);
+}
+
+console.log("rock");

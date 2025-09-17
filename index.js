a3086768778ccb4b8d9a8e85c697f630aedd6a0d
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-17 15:58:24 +0100

    Modified index.js
    Added Computers Random generated choice

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Its all working");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let ComputerChoice= null;
+    let genNum = null;
+   genNum = Math.floor(Math.random()* 3) + 1 ;
+   console.log(genNum);
+
+   if (genNum == 0) {
+   ComputerChoice = "Rock" ;
+}   else if(genNum == 1){
+   ComputerChoice = "paper" ;
+} else {
+   ComputerChoice = "Scissors" ;
+}
+    return ComputerChoice;
+}

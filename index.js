2a4e2a9f921990b3e42abbf6b0a124f43bc6e245
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-18 14:47:14 +0100

    modified index.js
    Added the logic and function to be able to play the round and also making sure 5 rounds were played in a game#

--- index.js
+++ index.js
@@ -2,33 +2,67 @@
 let humanScore = 0;
 let computerScore = 0;
 
+
 function getComputerChoice(){ //calls a random generated number which converts to a choice
-    let ComputerChoice= null;
+    let computerSelection= null;
     let genNum = null;
    genNum = Math.floor(Math.random()* 3) + 1 ;
-   console.log(genNum);
 
    if (genNum == 0) {
-   ComputerChoice = "rock" ;
+   computerSelection = "rock" ;
 }   else if(genNum == 1){
-   ComputerChoice = "paper" ;
+   computerSelection = "paper" ;
 } else {
-   ComputerChoice = "scissors" ;
+   computerSelection = "scissors" ;
 }
-    console.log(typeof ComputerChoice);
-    return ComputerChoice;
+
+   console.log(computerSelection);
+    return computerSelection;
+ 
 }
 
 function getHumanChoice(){
-    let HumanChoice = null;
+    let humanSelection = null;
     let input = null; 
     while (!(input=="rock"||input=="paper"||input=="scissors")){
      input= prompt("Pick one of the 3 Options listed \n (Rock, Paper or Scissors)") ;
         input = input.toLowerCase();
-     HumanChoice = input.toLowerCase();}
+     humanSelection = input.toLowerCase();}
 
-    console.log(HumanChoice);
-    console.log(typeof HumanChoice);
+    console.log(humanSelection);
+    return humanSelection
 }
 
-console.log("rock");
+function playRound(humanSelection, computerSelection){
+    if (humanSelection == "paper" && computerSelection == "rock"||humanSelection == "rock" && computerSelection == "scissors"||humanSelection == "scissors" && computerSelection == "paper"){
+      console.log("You have won as" + " " + humanSelection + " "+ "beats"+" " + computerSelection)
+      humanScore= 1+humanScore;
+    } else if (humanSelection==computerSelection){
+      console.log("You have both picked the same so no one gets a point. ")
+    } else {
+      console.log("You have lost as" + " " + computerSelection + " "+ "beats"+" " + humanSelection)
+      computerScore= 1+computerScore;
+
+    }
+
+}
+
+function playGame(){
+   while(!(humanScore==5||computerScore==5)){
+      let i = 1;
+      console.log("Round " + i )
+      console.log("You have picked:");
+let humanSelection = getHumanChoice();
+console.log("The computer has picked:");
+let computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+console.log("Your current score is "+humanScore+" "+"and the computers score is currently "+computerScore)
+i++;
+   }
+   if(humanScore==5){
+      console.log("You have Won with 5 points")
+   }else{
+      console.log("You have lost with " + humanScore)
+   }
+}
+   playGame();

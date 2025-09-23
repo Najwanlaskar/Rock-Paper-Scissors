7a23a1e9b818f67c31545581b9d558a3112d809b
Najwanlaskar <Najwanlaskar12@example.com>
2025-09-23 23:21:49 +0100

    modified.html and index.js
    Added div to html and for jsi added logic to display the console.logs for better user experience

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
 let humanScore = 0;
 let computerScore = 0;
-
+let humanSelection = null
+    let computerSelection= null;
 
 function getComputerChoice(){ //calls a random generated number which converts to a choice
-    let computerSelection= null;
     let genNum = null;
    genNum = Math.floor(Math.random()* 3) + 1 ;
 
-   if (genNum == 0) {
+   if (genNum == 1) {
    computerSelection = "rock" ;
-}   else if(genNum == 1){
+}   else if(genNum == 2){
    computerSelection = "paper" ;
 } else {
    computerSelection = "scissors" ;
@@ -22,7 +22,6 @@ function getComputerChoice(){ //calls a random generated number which converts t
 }
 
 function getHumanChoice(){
-    let humanSelection = null;
     let input = null; 
     while (!(input=="rock"||input=="paper"||input=="scissors")){
      input= prompt("Pick one of the 3 Options listed \n (Rock, Paper or Scissors)") ;
@@ -47,22 +46,112 @@ function playRound(humanSelection, computerSelection){
 
 }
 
-function playGame(){
-   while(!(humanScore==5||computerScore==5)){
-      let i = 1;
-      console.log("Round " + i )
-      console.log("You have picked:");
-let humanSelection = getHumanChoice();
-console.log("The computer has picked:");
+
+
+
+const div = document.querySelector("#BtnList")
+
+let rockBtn=document.createElement("button")
+rockBtn.textContent="Rock";
+rockBtn.addEventListener("click",(e)=>{
+console.log("Rock clicked")
 let computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-console.log("Your current score is "+humanScore+" "+"and the computers score is currently "+computerScore)
-i++;
-   }
-   if(humanScore==5){
-      console.log("You have Won with 5 points")
-   }else{
-      console.log("You have lost with " + humanScore)
-   }
+playRound("rock",computerSelection);
+humanSelection="rock"
+if (!document.querySelector("#playagain")) {
+playgame() }
+})
+div.appendChild(rockBtn);
+
+
+let paperBtn=document.createElement("button")
+paperBtn.textContent="Paper";
+paperBtn.addEventListener("click",(e)=>{
+console.log("paper clicked")
+let computerSelection = getComputerChoice();
+humanSelection="paper"
+playRound("paper",computerSelection);
+if (!document.querySelector("#playagain")) {
+playgame()}
+
+})
+div.appendChild(paperBtn);
+
+let scissorsBtn=document.createElement("button")
+scissorsBtn.textContent="Scissors";
+scissorsBtn.addEventListener("click",(e)=>{
+console.log("Scissors clicked")
+let computerSelection = getComputerChoice();
+playRound("scissors",computerSelection);
+humanSelection="scissors"
+if (!document.querySelector("#playagain")) {
+playgame()}
+})
+div.appendChild(scissorsBtn);
+
+let vs=document.createElement("p");
+vs.textContent= "You haven't picked yet"
+div.append(vs);
+
+let PlayerScore=document.createElement("p");
+PlayerScore.textContent= "Your score is current: " + humanScore
+
+let CompScore=document.createElement("p");
+CompScore.textContent= " The computers score is current: " + computerScore
+
+div.append(PlayerScore);
+div.append(CompScore);
+
+//div for displaying running score -- humanScore and computerScore
+//div for announcing winner --
+//logic for 5 rounds
+
+function playgame(){
+
+   CompScore.textContent= " The computers score is current: " + computerScore
+   PlayerScore.textContent= "Your score is current: " + humanScore
+   vs.textContent= "You picked " + humanSelection + " and the Computer chose " + computerSelection
+
+   if (humanScore==5){
+      
+let win=document.createElement("p");
+win.textContent= "You have won with  " + humanScore + " points"
+div.append(win);
+
+let playagain = document.createElement("button");
+playagain.textContent ="Play Again"
+playagain.id = "playagain"; // give it an ID to detect duplicates
+
+div.appendChild(playagain);
+
+playagain.addEventListener("click",(e)=>{
+
+div.removeChild(win)
+humanScore=0;
+computerScore =0;
+div.removeChild(playagain)
+
+})
+
+   }else if(computerScore==5){
+      let lost=document.createElement("p");
+lost.textContent= "You have lost with  " + humanScore + " points"
+div.append(lost);
+
+let playagain = document.createElement("button");
+playagain.textContent ="Play Again"
+playagain.id = "playagain"; // give it an ID to detect duplicates
+
+div.appendChild(playagain);
+
+playagain.addEventListener("click",(e)=>{
+
+div.removeChild(lost)
+humanScore=0;
+computerScore =0;
+div.removeChild(playagain)
+
+})
+   } else { }
+
 }
-   playGame();
